361224f15c6549e18766d6188986564d258e33e7
ninjjja91 <47461281+ninjjja91@users.noreply.github.com>
2023-05-20 07:58:57 +0200

    Add files via upload

---
+++ skel/zsh_p10k.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
