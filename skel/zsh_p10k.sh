482b7c00aa9a3259e287d543245543df65a0a7ce
ninjjja91 <47461281+ninjjja91@users.noreply.github.com>
2024-02-02 05:53:43 +0100

    Delete skel directory

--- skel/zsh_p10k.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
