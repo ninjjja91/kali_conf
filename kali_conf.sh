5cd0c494e88de796e57e3f23ce7fc430cda8f4c8
ninjjja91 <47461281+ninjjja91@users.noreply.github.com>
2024-02-22 14:50:36 +0000

    Add files via upload

---
+++ kali_conf.sh
@@ -0,0 +1,10 @@
+sudo apt update
+
+sudo apt install kali-desktop-i3 terminator i3blocks feh grub-customizer ranger awesome-extra dunst 
+
+cp -r .config/* ~/.config/
+cp -r .fehbg ~/.fehbg
+cp -r .fonts ~/.fonts
+cp -r .wallpapers ~/.wallpapers
+cp -r .xinitrc ~/.xinitrc
+
