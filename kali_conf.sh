7f9dac92cb62985d45e09b998ab3feeba2b78fc9
ninjjja91 <47461281+ninjjja91@users.noreply.github.com>
2024-03-03 05:50:52 +0000

    Update kali_conf.sh

--- kali_conf.sh
+++ kali_conf.sh
@@ -1,10 +1,10 @@
 sudo apt update
 
 sudo apt install kali-desktop-i3 terminator i3blocks feh grub-customizer ranger awesome-extra dunst 
-
+sudo apt purge xdg-desktop-portal
 cp -r .config/* ~/.config/
 cp -r .fehbg ~/.fehbg
 cp -r .fonts ~/.fonts
 cp -r .wallpapers ~/.wallpapers
 cp -r .xinitrc ~/.xinitrc
-
+sudo systemctl disable lightdm.service
